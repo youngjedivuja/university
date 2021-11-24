2faec9bbaa25037a6b2fef9241ec608ac9fe42ba
youngjedivuja <jvujovic1998@gmail.com>
2021-11-24 22:48:07 +0100

    Finished two homeworks for c# course

---
+++ seventh_semester/cs322/homeworks/hw02/CS322-DZ02-3860-JovanVujovic/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CS322_DZ02_3860_JovanVujovic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string info = "";
+
+            info += "Ime: " + textBox1.Text + "; ";
+            info += "Prezime: " + textBox2.Text + "; ";
+            info += "Adresa: " + textBox3.Text + "; ";
+
+            MessageBox.Show(info);
+
+            using (StreamWriter sw = new StreamWriter("out.txt")) {
+                sw.Write(info);
+            }
+        }
+    }
+}
