7ec3a1bdc2525361a285998a7127bbf23aa5267f
vuja <jvujovic1998@gmail.com>
2022-03-23 21:36:27 +0100

    finished 15th homework for c#

---
+++ seventh_semester/cs322/homeworks/hw15/CS322-DZ15-3860-JovanVujovic/Program.cs
@@ -0,0 +1,27 @@
+﻿int time;
+float rate, cash, interest;
+
+void Main() {
+    //cash input
+    Console.WriteLine("Enter cash amount: ");
+    cash = Convert.ToSingle(Console.ReadLine());
+    if (cash <= 0)
+        throw new ArgumentException("Cash must be positive");
+    
+    //time input
+    Console.WriteLine("Enter amount of days for calculating your interest: ");
+    time = Convert.ToInt32(Console.ReadLine());
+    if (time <= 0)
+        throw new ArgumentException("The amount of days for calculating the interest must be positive");
+    
+    //Interest rate input
+    Console.WriteLine("Enter the interest rate to calculate: ");
+    rate = Convert.ToSingle(Console.ReadLine());
+    if (rate < 0)
+        throw new ArgumentException("Rate cannot be negative");
+
+    interest = cash * time * rate / 100;
+    Console.WriteLine("Your calculated interest is: " + interest);
+}
+
+Main();
